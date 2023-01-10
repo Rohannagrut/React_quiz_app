3badea75e34d6c21fef43c8b1afedbb722d760e0
Rohannagrut <rgnagrut_b20@ee.vjti.ac.in>
2023-01-10 21:27:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react';
 
-function App() {
+export default function App() {
+  const questions = [
+    {
+      questionText: 'What is the capital of France?',
+      answerOptions: [
+        { answerText: 'New York', isCorrect: false },
+        { answerText: 'London', isCorrect: false },
+        { answerText: 'Paris', isCorrect: true },
+        { answerText: 'Dublin', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Who is CEO of Tesla?',
+      answerOptions: [
+        { answerText: 'Jeff Bezos', isCorrect: false },
+        { answerText: 'Elon Musk', isCorrect: true },
+        { answerText: 'Bill Gates', isCorrect: false },
+        { answerText: 'Tony Stark', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'The iPhone was created by which company?',
+      answerOptions: [
+        { answerText: 'Apple', isCorrect: true },
+        { answerText: 'Intel', isCorrect: false },
+        { answerText: 'Amazon', isCorrect: false },
+        { answerText: 'Microsoft', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'How many Harry Potter books are there?',
+      answerOptions: [
+        { answerText: '1', isCorrect: false },
+        { answerText: '4', isCorrect: false },
+        { answerText: '6', isCorrect: false },
+        { answerText: '7', isCorrect: true },
+      ],
+    },
+  ];
+
+  const [currQuestion, setCurrentQuestion] = useState(0);
+
+  const [showScore, setShowScore] = useState(false);
+
+  const [score, setScore] = useState(0);
+
+  const resetStateClick = () => {
+    setCurrentQuestion(0)
+    setShowScore(false)
+    setScore(0)
+  }
+  
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    }
+    else {
+      setShowScore(true);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {
+      /* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+      {/* if showScore is true show this stuff  showscore ? ():() */}
+      {showScore ? (
+        <div className='score-section'>You scored {score} out of {questions.length}
+          <button onClick={resetStateClick}>Retry</button>
+        </div>
+      ) : (
+        <>
+          {/* if showScore is false then show this  */}
+          <div className='question-section'>
+            <div className='question-count'>
+              <span>Question {currQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className='question-text'>{questions[currQuestion].questionText}</div>
+          </div>
+          <div className='answer-section'>
+            {questions[currQuestion].answerOptions.map((answerOptions) =>
+              <button onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)} >{answerOptions.answerText}</button>)}
+          </div>
+          
+        </>
+      )}
     </div>
   );
-}
-
-export default App;
+} 
